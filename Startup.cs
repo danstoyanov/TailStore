72851e8d3ec8d6494889c5597dcd7c146ab351bb
danstoyanov <dan.stoyanov@abv.bg>
2023-07-19 20:06:43 +0300

    ⚠️ Add Main Project Structure

--- Startup.cs
+++ Startup.cs
@@ -29,13 +29,13 @@ namespace TailStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<TailStoreDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<TailStoreDbContext>();
             services.AddControllersWithViews();
         }
 
